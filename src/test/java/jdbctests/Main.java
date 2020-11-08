c6c9a32a568d049340190e2486373e6cf7d94e97
Mehmet <mhmtsytrk@gmail.com>
2020-11-08 22:08:14 +0300

    jdbc

---
+++ src/test/java/jdbctests/Main.java
@@ -0,0 +1,8 @@
+package jdbctests;
+public class Main {
+    public static void main(String[] args) {
+        String dbUrl = "jdbc:oracle:thin:@107.21.153.23:1521:xe";
+        String dbUsername = "hr";
+        String dbPassword = "hr";
+    }
+}
